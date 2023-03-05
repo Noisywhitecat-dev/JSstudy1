46aa097e6de538de4230c4114084e44e69dc7034
noisywhitecat <noisywhitecat@gmail.com>
2023-03-05 17:53:54 +0900

    Changes

--- test1.js
+++ test1.js
@@ -1,5 +1,20 @@
-function hello(name) {
-    console.log(`Hello, ${name}!`);
+const ironMan = {
+    name: '토니 스타크',
+    actor: '로버트 다우니 주니어',
+    alias: '아이언맨'
+};
+
+const captainAmerica = {
+    name: '스티븐 로저스',
+    actor: '크리스 에반스',
+    alias: '캡틴 아메리카'
+};
+
+function print(hero) {
+    const {alias, name, actor} = hero;
+    const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
+    console.log(text);
 }
-hello('noisywhitecat');
-console.log('sdad');
+
+print(ironMan);
+print(captainAmerica);
