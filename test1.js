b83d0644bbd3772d2618d0358cdfc99517a009f6
noisywhitecat <noisywhitecat@gmail.com>
2023-03-12 22:43:40 +0900

    Changes

--- test1.js
+++ test1.js
@@ -1,25 +1,12 @@
-const todos = [
-    {
-        id: 1,
-        text: '자바스크립트 입문',
-        done: true
-    },
-    {
-        id: 2,
-        text: '함수 배우기',
-        done: true
-    },
-    {
-        id: 3,
-        text: '객체와 배열 배우기',
-        done: true
-    },
-    {
-        id: 4,
-        text: '배열 내장함수 배우기',
-        done: false
-    }
-];
+function countBiggerThanTen(numbers) {
+    let cnt = numbers.reduce((res, cval) => {
+        if (cval > 10) {
+            return res + 1;
+        }
+        else return res;
+    }, 0);
+    return cnt;
+}
 
-const index = todos.find(todo => todo.id === 3);
-console.log(index);
+const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
+console.log(count); // 5
